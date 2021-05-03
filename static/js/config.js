0926a6728feefe79b54196511a6673845db4de4b
kerryharp <kharp@umn.edu>
2021-05-02 22:48:51 -0500

    lots of changes, map with mouseon/off working, placeholders as per Saturday's discussion, walkscore data

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoia2hhcnAiLCJhIjoiY2tucnVkbzVyMDBqcDJwdGhqMWNhYjNpMCJ9._skl8-iwvEWT_dZ4P4jxFw";
+const API_KEY = "pk.eyJ1Ijoia2hhcnAiLCJhIjoiY2tvNGF0dzRhMGhkbTJucXdyd2VwZmNyaSJ9.Yal9ihBGNd-aNjx2nL8tIQ";
